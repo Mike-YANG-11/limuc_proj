a03265793bdffa0fbb4b35dcf290ca053b65a06a
Mike-YANG-11 <detactive.x.x@gmail.com>
2024-09-08 22:33:03 +0800

    .

---
+++ models/__init__.py
@@ -0,0 +1,24 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+# --------------------------------------------------------
+
+from .hiera import (
+    hiera_tiny_224,
+    hiera_small_224,
+    hiera_base_224,
+    hiera_base_plus_224,
+    hiera_large_224,
+    hiera_huge_224,
+    hiera_base_16x224,
+    hiera_base_plus_16x224,
+    hiera_large_16x224,
+    hiera_huge_16x224,
+    Hiera,
+    HieraBlock,
+    MaskUnitAttention,
+    Head,
+    PatchEmbed,
+)
